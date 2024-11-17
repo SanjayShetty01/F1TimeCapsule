fa56a6c907b72ab028698594cce90b61bd4879c1
shettysanjay01 <shettysanjay01@gmail.com>
2024-11-17 10:09:13 +0530

    adding db support

--- data_processor/src/main/scala/main.scala
+++ data_processor/src/main/scala/main.scala
@@ -5,8 +5,6 @@ def main(): Unit = {
   downloadZipFile(url = "https://www.kaggle.com/api/v1/datasets/download/rohanrao/formula-1-world-championship-1950-2020",
     targetPath = "downloads/dataset.zip")
   unzipFile(zipFilePath = "downloads/dataset.zip", outputDir = "downloads/data/")
-
-
-  print("downloaded")
+  print("LFGGGGGGGGGGGG")
 }
 
